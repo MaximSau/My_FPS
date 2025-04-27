482b5636ff86aca16dcd7f7843d0d01cd3ea8faa
MaximSau <clashminiclashmini016@gmail.com>
2025-04-27 23:21:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ enemy = AdvancedEnemy(position=(5, 0, 5))
 
 # карта
 map_entity = Entity(
-    model='textures/de_dust2.obj',
-    collider='mesh',
-    texture='white_cube',
+    model=load_model('textures/de_dust2.obj', use_deepcopy=True),
     scale=0.03,
     rotation=(-90,0,0),
     position=(25, 0, 0),
-    double_sided=True  # Важно для внутренних поверхностей!
+    texture='white_cube',
+    collider='mesh',
+    double_sided=True
 )
 map_entity.combine()
 
 #рука
 hand = Entity(
     parent=camera.ui,
-    model='arm',
+    model=load_model('arm', use_deepcopy=True),
     texture='arm_tex',
     scale=0.23,
     position=Vec3(0.63, -0.34, -0.1),
@@ -43,7 +43,7 @@ hand = Entity(
 #оружие
 gun = Entity(
     parent=camera.ui,
-    model='USP',
+    model=load_model('USP', use_deepcopy=True),
     texture='dirty-metal-texture',
     scale=0.07,
     position=Vec3(0.4, -0.3, 0.27),
@@ -52,7 +52,7 @@ gun = Entity(
 
 
 
-DirectionalLight(position=(10, 20, -10), shadows=True)
-AmbientLight(color=color.gray)
+DirectionalLight(position=(10, 20, -10), shadows=False)  # Отключение теней
+AmbientLight(color=color.dark_gray)
 
 app.run()
